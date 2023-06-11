4be5e25e4bbccf5f3910632b2b6ac8ddf1e5dfe1
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-11 19:43:20 +0530

    simpleOne

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
 
-import { MongoClient } from "mongodb";
 
 async function main() {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  let db = client.db("mydb");
-  let messageCollection = db.collection("message");
-
-  let inputDocument = { message: "helloo" };
-  await messageCollection.insertOne(inputDocument);
-  console.log("sucess");
-
-  await client.close();
+ 
+  return "Hello World";
+  
 }
 
-main();
+
 
