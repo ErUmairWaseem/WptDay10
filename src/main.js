ffa3c01ee1890317b98eb2194f7a78df7cbbaea0
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-11 08:35:48 +0530

    passing data as argument

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
 import { MongoClient } from "mongodb";
 
-async function insertRecord() {
+async function insertRecord(jsonDocument) {
   const uri = "mongodb://127.0.0.1:27017";
-  // open the connection
+ 
   const client = new MongoClient(uri);
 
   const db = client.db("umairdb");
   const message = db.collection("message");
 
-  let jsonDocument = {message:"Hello World", to:"Umesh", from:"Umair"};
+  //let jsonDocument = {message:"Hello World", to:"Umesh", from:"Umair"};
   await message.insertOne({jsonDocument});
 
-  // close the connection
   await client.close();
   console.log("Record Added");
 }
 
 async function main(){
-await insertRecord();
+  let jsonDocument = {
+  message:"Hello guys", 
+  to:"Umesh", 
+  from:"Umair"
+};
+
+ await insertRecord(jsonDocument);
 
 }
 main();
