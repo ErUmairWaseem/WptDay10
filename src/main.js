f11a2614e0d7bd43afd91868ed7d804b3231fffc
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-11 21:03:56 +0530

    revise network calling

--- src/main.js
+++ src/main.js
@@ -4,33 +4,16 @@ import  express, { application }  from "express";
 const app = express();
 
 
-
-
 //if you want sent some message 
-function main(req , res){
-   res.send("Hellooouuuu...!!");
+function my(req , res){
+   res.send("Hello...!!");
 }
-app.get("/main",main)
+app.get("/my",my);
 
 
-
-function first(req,res) {
-
-  //write logic here
-  res.send("My Name is Umair")
-}
-app.get("/first",first)
-
-
-function second(req,res) {
-  res.send("CDAC Mumbai(Kharghar)");
-}app.get("/second", second)
-
-
-
-
-
-
+app.get("/main",(req, res) => {
+  res.send("Hello Callback...!");
+})
 
 app.listen(4000);
 
