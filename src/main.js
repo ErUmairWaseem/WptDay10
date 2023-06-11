a29fa29f16468c937a8f01213dfbc9de2c4a979b
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-11 19:23:00 +0530

    insertagain

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 
-import {  } from "./umairdb.js";
+import { MongoClient } from "mongodb";
 
-async function main(){
- 
-  let jsonDocument = {
-  message:"Hello guys", 
-  to:"Umesh", 
-  from:"Umair"
-};
+async function main() {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
 
- let list = await readRecord();
-console.log(list);
+  let db = client.db("mydb");
+  let messageCollection = db.collection("message");
+
+  let inputDocument = { message: "helloo" };
+  await messageCollection.insertOne(inputDocument);
+  console.log("sucess");
+
+  await client.close();
 }
 
-main();
 
